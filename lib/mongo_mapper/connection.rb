31a24bdb007c967e46ca7c5cb8ca03dc0c41f368
Arne Hartherz <arne.hartherz@makandra.de>
2013-03-01 14:18:29 +0100

    Revert changes for replica sets (tests were failing)
    `ReplSetConnection` is unavailable which causes tests to break.
    
    -------------------------------------------------------------------------------
    
    Revert "Write a better test, making sure ReplSetConnection is passed options hash"
    This reverts commit 27322365c01a72174e8e74637796772c31489cd7.
    
    Revert "Added support for a Replica Set"
    This reverts commit 8db1054a41c76a6fcd6cc3b382f408c2b4fc37b7.
    
    Revert "Add test for replica set connections from config that specifies multiple hosts"
    This reverts commit 7d724f3cf7544f31441c84cba880aeb13d8aab4c.

--- lib/mongo_mapper/connection.rb
+++ lib/mongo_mapper/connection.rb
@@ -61,13 +61,7 @@ module MongoMapper
     def connect(environment, options={})
       raise 'Set config before connecting. MongoMapper.config = {...}' if config.blank?
       env = config_for_environment(environment)
-
-      MongoMapper.connection = if env['hosts']
-        Mongo::ReplSetConnection.new( *env['hosts'].push(options) )
-      else
-        Mongo::Connection.new(env['host'], env['port'], options)
-      end
-
+      MongoMapper.connection = Mongo::Connection.new(env['host'], env['port'], options)
       MongoMapper.database = env['database']
       MongoMapper.database.authenticate(env['username'], env['password']) if env['username'] && env['password']
     end
